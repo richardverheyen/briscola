272c06fc93227003a426faaeb449860bd02ebe66
Richard <rv@floatplane.dev>
2022-06-27 14:46:56 +0100

    Add a loading state to the create game button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,51 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Auth } from "contexts";
 import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  let { signIn, signOut, data } = useContext(Auth);
+  let { data } = useContext(Auth);
+  let [isLoading, setIsLoading] = useState(false);
   let navigate = useNavigate();
 
   const createGame = () => {
+    setIsLoading(true);
     fetch(
       // 'http://localhost:5001/briscola-fp/us-central1/createGame',
-      'https://us-central1-briscola-fp.cloudfunctions.net/createGame',
+      "https://us-central1-briscola-fp.cloudfunctions.net/createGame",
       {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
         headers: {
-          'Content-Type': 'application/text'
+          "Content-Type": "application/text",
         },
-        body: JSON.stringify({}) // body data type must match "Content-Type" header
+        body: JSON.stringify({}), // body data  type must match "Content-Type" header
       }
     )
-      .then(res => res.json()) // must match match "Content-Type" header
-      .then(data => {
-        console.log('Created Game', data.id);
+      .then((res) => res.json()) // must match match "Content-Type" header
+      .then((data) => {
+        // console.log('Created Game', data.id);
         navigate(`game/${data.id}`);
       })
       .catch((err) => {
-        console.error('Create Game Failed', {err});
+        console.error("Create Game Failed", { err });
       })
+      .finally(() => {
+        setIsLoading(true);
+      });
   };
 
   return (
     <div className="Home">
-      {data ? (
-        <Button onClick={signOut} variant="outlined">
-          Log Out
-        </Button>
-      ) : (
-        <Button onClick={signIn} variant="outlined">
-          Log In
-        </Button>
-      )}
-
-      <Button disabled={!data} onClick={createGame} variant="contained">
+      <LoadingButton
+        disabled={!data}
+        loading={isLoading}
+        onClick={createGame}
+        variant="contained"
+      >
         Create Game
-      </Button>
+      </LoadingButton>
     </div>
   );
 }
